635a20b3407b3b19096141df951cef25f6d615ae
luis-vanin <vanin@copybase.com.br>
2022-11-27 00:07:23 -0300

    feat: creating item

--- postgres_to_json/src/main.rs
+++ postgres_to_json/src/main.rs
@@ -1,5 +1,11 @@
-use postgres::{Client, Column, NoTls, Row};
-use std::io::stdin;
+use postgres::{types::FromSql, Client, Column, NoTls, Row};
+use std::{
+    borrow::Borrow,
+    env,
+    fmt::Display,
+    fs::File,
+    io::{stdin, Write},
+};
 
 #[derive(Debug)]
 struct UserData {
@@ -15,9 +21,10 @@ enum Data {
     Str(String),
 }
 
-type SqlData = Option<Data>;
-
 fn main() {
+    let mut arguments = env::args();
+    let root_path = arguments.next().unwrap();
+    let target_path = arguments.next().get_or_insert("./".into());
     let user = read_user_data();
     println!("{:?}", user.get_db_url());
     let conn = Client::connect("postgresql://postgres:1337@localhost:5432/boardcamp", NoTls);
@@ -32,17 +39,21 @@ fn main() {
                 let rows = client
                     .query(format!("select * from {}", table_name).as_str(), &[])
                     .unwrap();
-                // println!("{}", row)
-                println!("{:?}", table_name);
-                for i in rows {
-                    // println!("{}", i.len());
-                    // let value: SqlData = i.get(0);
-                    // println!("{}", value);
-                    // for j in 0..i.len() {
-                    //     let value: String = i.get(j);
-                    //     let column: String = i.columns()[j].name().into();
-                    //     println!("{}: {}", column, value);
-                    // }
+                println!("---------{:?}", table_name);
+                for row in &rows {
+                    let columns = row.columns().clone();
+                    for j in columns {
+                        println!("name: {}, type: {}", j.name(), j.type_().name());
+                        let value: Option<String> = format_row_to_string(row, j);
+                        match value {
+                            Some(json) => {
+                                println!("{}", json)
+                            }
+                            None => {
+                                println!("I dont know how to parse to shit")
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -51,6 +62,10 @@ fn main() {
             panic!("Ih deu ruim pai")
         }
     }
+}
+
+fn create_object(json_object: &mut String) -> String {
+    format!("{{ {} }}", json_object)
 }
 
 fn read_line_clean(input: &mut String, default: &str) {
@@ -84,6 +99,29 @@ fn read_user_data() -> UserData {
     read_line_clean(&mut user.port, "5432");
 
     return user;
+}
+
+fn convert_sql_to_json<'a, T: FromSql<'a> + Display>(row: &'a Row, column_name: &str) -> String {
+    let value: Option<T> = row.get(column_name);
+    return match value {
+        Some(v) => {
+            format!("\"{}\": {}", column_name, v)
+        }
+        None => {
+            format!("\"{}\": null", column_name)
+        }
+    };
+}
+
+fn format_row_to_string(row: &Row, column: &Column) -> Option<String> {
+    match column.type_().name() {
+        "int4" => Some(convert_sql_to_json::<i32>(row, column.name())),
+        "text" => Some(convert_sql_to_json::<&str>(row, column.name())),
+        _ => {
+            println!("I dont know how to parse this shit!");
+            None
+        }
+    }
 }
 
 impl UserData {
